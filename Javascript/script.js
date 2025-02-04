5a858fd348fafdc34533d1ebf3bb47161d6f1db5
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-04 14:47:29 +0000

    tipode_de_dados

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,23 @@
-    alert("Olá, mundo!");
+   console.info(
+    "Esta é uma mensagem disparada pelo JS para o console do navegador"
+   );
+
+   console.log(50.95); //Number
+   console.log("Teste"); //String (texto)
+   console.log(true, false); //Boolean (true ou false)
+
+   let indefinido;
+
+   console.log (indefinido); //undefined (indefinido)
+
+   console.log(null); //null (nulo)
+
+   console.log(BigInt(16518948151615158184151651968481));
+
+   let pessoa = {
+    //Object (Objeto)
+    nome: "Vitor",
+    idade: 23,
+   };
+
+   console.log(pessoa);
