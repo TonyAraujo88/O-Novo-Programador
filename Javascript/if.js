eeb92b1404bd821a9543ffcfc35b2e6bdacd056e
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-04 20:06:23 +0000

    if_else_parte2

--- Javascript/if.js
+++ Javascript/if.js
@@ -4,9 +4,12 @@ const senha = "senha123";
 
 // 1 sinal de = (igual) é atribuição, definindo algo.
 // 2 sinais de = (igual) é comparação.
+// 3 sinais de = (igual) é comparação de tipagem.
 
 //no if && = E
 //no if || = OU
+//no if > = MAIOR
+//no if < = MENOR
 
 if (email == "contatoonovoprogramador.com" || senha == "senha123") {
     //Fazer login do ususário
@@ -15,16 +18,29 @@ if (email == "contatoonovoprogramador.com" || senha == "senha123") {
 
 const prato = "vazio";
 
-let login = false
-
-if (login == true) {
-    console.log("teste");
+if (prato == "vazio") {
+    console.log("Retirar prato!")
 }
 
-if (prato == "vazio"){
-    console.log("Retirar Prato")
+if (prato !="cheio") {
+    console.log("Retirar prato!");
 }
 
-if (prato !="vazio") {
-    console.log("Não retirar o prato!");
+const numero1 = 50;
+const numero2 = 50;
+
+if (numero1 == numero2) {
+    console.log("Os numeros são iguais!");
+    console.log(numero1 + numero2);
+}
+
+const score_serasa = 600;
+
+
+if (score_serasa >= 600 && score_serasa <= 800) {
+    console.log("Aprovado o financiamento Parabéns!");
+}
+
+if (numero1 < numero2) {
+    console.log("O segundo número é maior");
 }
