2f7ff6e836283c35a3fb808af7b9578e0bc92146
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-04 19:29:08 +0000

    if_else_parte1

---
+++ Javascript/if.js
@@ -0,0 +1,30 @@
+const email = "contatoonovoprogramador.com"; 
+const senha = "senha123";
+
+
+// 1 sinal de = (igual) é atribuição, definindo algo.
+// 2 sinais de = (igual) é comparação.
+
+//no if && = E
+//no if || = OU
+
+if (email == "contatoonovoprogramador.com" || senha == "senha123") {
+    //Fazer login do ususário
+    console.log("Sim o e-mail e a senha estão corretos");
+}
+
+const prato = "vazio";
+
+let login = false
+
+if (login == true) {
+    console.log("teste");
+}
+
+if (prato == "vazio"){
+    console.log("Retirar Prato")
+}
+
+if (prato !="vazio") {
+    console.log("Não retirar o prato!");
+}
