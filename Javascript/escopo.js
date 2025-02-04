25c84d2a8699e2a20bd4c5f402f61587c4e10651
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-04 17:54:07 +0000

    escopo

---
+++ Javascript/escopo.js
@@ -0,0 +1,23 @@
+//Tudo o que vai estar escrito aqui dentro já forma um escopo.
+
+let nome ="Vitor";
+let email ="contato@onovoprogrmador.com"; 
+
+
+    function retornarNome() {
+        return nome;
+    }
+
+    function retornarIdade() {
+        let idade = 23;
+    }
+
+    if (nome === "Vitor") {
+        let idade = 23;
+    }
+
+console.log(idade);
+
+console.log(retornarIdade());
+
+console.log(retornarNome());
---
+++ Javascript/variaveis.js
@@ -0,0 +1,10 @@
+const API_URL = "https//api.onovoprogramador.com";
+
+let especie1 = "Onça";
+
+especie = "Macaco";
+
+especie = "Canário";
+
+console.log(especie);
+console.log(especie1);
