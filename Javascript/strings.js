e99f2bda7a785accc0f2b8e84e6b23d74c9af4c6
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-04 18:28:39 +0000

    IF

---
+++ Javascript/strings.js
@@ -0,0 +1,11 @@
+//Sequência de caracteres para reprensentar um texto, interpretado pelo JavaScript com as áspas.
+
+let nome = "Vitor";
+let sobrenome = "Manoel";
+let idade = 23;
+
+let nomeCompleto = nome + sobrenome;
+let nomeCompleto2 =  `Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos!`;
+
+console.log(nomeCompleto2);
+
