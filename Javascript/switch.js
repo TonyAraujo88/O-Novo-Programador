236a58a06fc8e4890162fd1d0638a4be4381b5d1
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-05 21:03:04 +0000

    switch1

--- Javascript/switch.js
+++ Javascript/switch.js
@@ -3,6 +3,14 @@
 const opcao = 10
 
 switch (opcao) {
+    case 10:
+        console.log("Feijão");
+    break;
+    case 20:
+        console.log("Carne");
+    break;
+    case 30:
+        console.log("Pizza");
 
-    
+
 }
