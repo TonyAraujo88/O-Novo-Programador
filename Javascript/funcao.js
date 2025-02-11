bc129bd26cf8e5e23f11d8615255a9349a7a8112
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-11 07:36:54 -0300

    funcao10

--- Javascript/funcao.js
+++ Javascript/funcao.js
@@ -5,3 +5,19 @@ function garcom(prato, mesa) {
 garcom("carbonara", "10");
 
 // A função só é executada quando eu a chamo
+
+const email_correto = "contato@onovoprogramador.com";
+const senha_correta = "senhadificil123";
+
+function login(email, senha) {
+    //Verificar s e-mail e senha estão corretos!
+}
+
+if(email === email_correto && senha === senha_correta) {
+ console.log("Login efeturado com sucesso");
+} else {
+    console.log("E-mail e/ou senha inválidos!");
+}
+
+login("contato@onovoprogramador.com", "123");
+
