8cc088d60d8ad269420ff69b7f3f21f788bb8f3e
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-11 08:14:20 -0300

    function

--- Javascript/funcao.js
+++ Javascript/funcao.js
@@ -2,7 +2,7 @@ function garcom(prato, mesa) {
     console.log("Entregar o prato para a mesa!", prato, mesa);
 }
 
-garcom("carbonara", "10");
+//garcom("carbonara", "10");
 
 // A função só é executada quando eu a chamo
 
@@ -10,14 +10,13 @@ const email_correto = "contato@onovoprogramador.com";
 const senha_correta = "senhadificil123";
 
 function login(email, senha) {
-    //Verificar s e-mail e senha estão corretos!
+    //Verificar se e-mail e senha estão corretos!
+
+    if (email === email_correto && senha === senha_correta) {
+        console.log("Login efetuado com sucesso!");
+    } else {
+        console.log("E-mail e/ou senha inválidos!");
+    }
 }
-
-if(email === email_correto && senha === senha_correta) {
- console.log("Login efeturado com sucesso");
-} else {
-    console.log("E-mail e/ou senha inválidos!");
-}
-
-login("contato@onovoprogramador.com", "123");
-
+ 
+login("contato@onovoprogramador.com", "senhadificil123");
