77a2518451b115f1560b7c1a49940b441b5829a4
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-13 01:27:37 -0300

    arrey

---
+++ Javascript/arrey.js
@@ -0,0 +1,19 @@
+const listaCompras = ["feijão", "arroz", "ovo"];
+
+const receitadePao = ["farinha de trigo", "fermento biológico", "açucar", "leite", "ovo"];
+
+listaCompras.push("macarrão");
+listaCompras.push("carne moida");
+
+listaCompras.unshift("frango")
+listaCompras.shift()
+
+//listaCompras.pop();
+
+console.log(listaCompras)
+
+// push - Adiciona um item do arrey no final do arrey (ultima posição)
+// unshift - Adiciona um item no array na primeira posição
+
+// pop - Exclui o último item do arrey
+// shift - Exclui o primeiro item do arrey
