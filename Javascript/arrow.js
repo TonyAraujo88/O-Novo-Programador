9c76df5d0f79eb3c20b6cd5db6d11891f4e4d70d
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-12 21:49:31 -0300

    arrow

---
+++ Javascript/arrow.js
@@ -0,0 +1,12 @@
+//function somar(numero1, numero2) {
+//    return numero1 + numero2;
+//}
+//console.log(somar(10, 50));
+
+const subtrair = (numero1, numero2) => {
+    let resultado = numero1 + numero2;
+    return resultado;
+}
+
+const multiplicar = (numero1, numero2) => numero1 * numero2;
+console.log(subtrair(50, 10));
