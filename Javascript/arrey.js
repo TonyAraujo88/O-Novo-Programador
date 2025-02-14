1fecd3c04ac3e12b92862ea693403786687402c8
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-14 19:46:45 +0000

    reduce

--- Javascript/arrey.js
+++ Javascript/arrey.js
@@ -1,9 +1,25 @@
 const vendasDoDia = [250, 800, 600, 200, 752, 2, 10, 30, 53, 71];
 
-let total = 0;
-vendasDoDia.map((valorVenda) => {
-    console.log(total);
-    total = total + valorVenda;
-})
+const total = vendasDoDia.reduce((acumulador, valorVenda) => {
+    return acumulador + valorVenda
+};
+    acumulador + valorVenda, 
+0);
 
-console.log(total)
+console.log(vendasDoDia)
+
+
+
+
+
+
+
+
+
+//let total = 0;
+//vendasDoDia.map((valorVenda) => {
+//    console.log(total);
+//    total = total + valorVenda;
+//});
+
+
