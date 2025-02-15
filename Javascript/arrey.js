d6121bc7119d3e9a6d071375de4a8a7c6b853228
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-14 21:52:53 -0300

    objeto

--- Javascript/arrey.js
+++ Javascript/arrey.js
@@ -1,12 +1,10 @@
 const vendasDoDia = [250, 800, 600, 200, 752, 2, 10, 30, 53, 71];
 
-const total = vendasDoDia.reduce((acumulador, valorVenda) => {
-    return acumulador + valorVenda
-};
-    acumulador + valorVenda, 
-0);
-
-console.log(vendasDoDia)
+//const total = vendasDoDia.reduce(
+//   (acumulador, valorVenda) => {
+//       return acumulador + valorVenda
+//   },
+//0);
 
 
 
@@ -16,10 +14,12 @@ console.log(vendasDoDia)
 
 
 
-//let total = 0;
-//vendasDoDia.map((valorVenda) => {
-//    console.log(total);
-//    total = total + valorVenda;
-//});
 
+
+let total = 0;
+vendasDoDia.map((valorVenda) => {
+    
+    total = total + valorVenda;
+});
+console.log(total)
 
---
+++ Javascript/objeto.js
@@ -0,0 +1,8 @@
+const pessoa = { nome: "Tony", qualificação: "ensino médio completo", idade: 36, religiao: "cistão", idade: 50,  };
+
+const pessoaArrey = [
+    { nome: "Tony", qualificação: "ensino médio completo", idade: 36, religiao: "cistão"  },   
+    { nome: "Luan", qualificação: "ensino médio completo", idade: 25, religiao: "cistão"  }  
+];
+
+console.log(pessoaArrey)
