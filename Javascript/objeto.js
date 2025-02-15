e094c083e24728581685e2f78f9634a7453e5a88
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-14 22:20:08 -0300

    manipulacao_objeto

--- Javascript/objeto.js
+++ Javascript/objeto.js
@@ -1,8 +1,15 @@
-const pessoa = { nome: "Tony", qualificação: "ensino médio completo", idade: 36, religiao: "cistão", idade: 50,  };
+const pessoa = { nome: "Tony", senha: "123", qualificação: "ensino médio completo", idade:  36, religião: "cistão", nivel_de_acesso: "admin" };
 
-const pessoaArrey = [
-    { nome: "Tony", qualificação: "ensino médio completo", idade: 36, religiao: "cistão"  },   
-    { nome: "Luan", qualificação: "ensino médio completo", idade: 25, religiao: "cistão"  }  
-];
 
-console.log(pessoaArrey)
+document.write(pessoa.nome)
+document.write(pessoa.qualificação)
+document.write(pessoa.idade)
+document.write(pessoa.religião)
+document.write(pessoa.nivel_de_acesso)
+
+pessoa.email = "contato@onovoprogramador.com";
+pessoa.nome = "Ana";
+
+delete pessoa.senha
+
+console.log(pessoa)
