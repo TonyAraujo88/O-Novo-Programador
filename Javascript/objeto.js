d3917e8ce940283216ea54109bbddc280df0e45e
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-02-19 15:50:00 +0000

    find_filter_map

--- Javascript/objeto.js
+++ Javascript/objeto.js
@@ -1,6 +1,6 @@
 const pessoa = {
     nome: "Vitor",
-    idade: 23,
+    idade: 24,
     interesses:["ciencia e tecnologia", "eletronicos"],
     conjuge: {
         nome: "Ana",
@@ -12,7 +12,8 @@ const pessoa = {
  const pessoas = [
     {
         nome: "Vitor",
-        idade: 23,
+        idade: 24,
+        religiao: 'católico',
         interesses:["ciencia e tecnologia", "eletronicos"],
         conjuge: {
             nome: "Ana",
@@ -21,8 +22,20 @@ const pessoa = {
         } 
     },
     {
+        nome: "Carlos",
+        idade: 24,
+        religiao: 'católico',
+        interesses:["ciencia e tecnologia", "eletronicos"],
+        conjuge: {
+            nome: "Luiza",
+            idade: 25,
+            interesses: ["maquiagem", "decoracao"]
+        } 
+    },
+    {
         nome: "Ana",
         idade: 25,
+        religiao: 'católico',
         interesses:["filosofia", "psicologia"],
         conjuge: {
             nome: "Vitor",
@@ -30,12 +43,27 @@ const pessoa = {
             interesses: ["ciencia e tecnologia", "eletronicos"]
         }
     },
-    ['teste']
+    
  ];
 
  //Não se esquecer de colocar a avírgula
 
-console.log(pessoas.find((item) => {
-    return item.nome === 'Ana';
-}))
+// find -> vai retornar o primeiro item do arrey que satisfaça a condição.
+//console.log(pessoas.find((item) => item.nome === 'Vitor'));
+
+//filter -> vai criar um arrey e retornar todos os itens que satisfaçam a condição da função
+//console.log(pessoas.filter((item) =>  item.idade === 24));
+
+//map -> vai mapear o arrey, pegar item a item, criar um arrey e retornar de acordo com uma função
+
+const resultado = pessoas.map((item, index) => {
+    const nomeAnterior = item.nome;
+   
+    item.nome = nomeAnterior.toUpperCase();
+    
+    return item;
+})
+
+console.log(resultado)
+
 
