01eb4e5a8d1ee7d78501d0b7f6298923ed35b99a
TonyAraujo88 <tonybugattieb@gmail.com>
2025-02-21 08:51:23 -0300

    notas2

--- Javascript/gerenciador_de_notas.js
+++ Javascript/gerenciador_de_notas.js
@@ -3,15 +3,25 @@
 
 const gerenciadorDeNotas = {
     notas: [
-        { titulo: 'levar o lixo', conteudo: 'A mulher já está brava!'}
+        { titulo: 'Levar o lixo', conteudo: 'A mulher já está brava!'}
     ],
 
     adicionarNota: function(tituloNovaNota, conteudoNovaNota) {
-        this.notas.push ({titlo: tituloNovaNota, conteudo: conteudoNovaNota});
+        this.notas.push({ titulo: tituloNovaNota, conteudo: conteudoNovaNota});
+    },
+
+    buscarNota: function(titulo) {
+       return this.notas.find((nota) => nota.titulo === titulo);
     }
 }
 
-gerenciadorDeNotas.adicionarNota('Estudar 4 aulas por dia', 'Modulo 2 de JS!');
-gerenciadorDeNotas.adicionarNota('Lembrar de tomar café', 'Você sempre deixa esfriar!');
+gerenciadorDeNotas.adicionarNota('Estudar 4 aulas por dia!', 'Módulo 2 de JS!');
+gerenciadorDeNotas.adicionarNota('Lembrar de tomar café!', 'Você sempre deixa esfriar!');
+gerenciadorDeNotas.adicionarNota('Comprar novo teclado', 'Este está horrível');
 
-console.log(gerenciadorDeNotas.notas)
+console.log(gerenciadorDeNotas.buscarNota('Estudar 4 aulas por dia!'));
+
+
+
+//console.log(gerenciadorDeNotas.notas)
+
