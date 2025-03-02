672d42c6bedd1c2bfe264bc00e0fcaa0b08230a6
TonyAraujo88 <tonybugattieb@gmail.com>
2025-03-02 09:51:59 -0300

    notas_editar_2

---
+++ Javascript/for.js
@@ -0,0 +1,16 @@
+const lista_compras = [
+        "Feijão",
+        "Arroz",
+        "Macarrão",
+        "Farinha"
+];
+
+lista_compras[2] = "batata";
+ console.log(lista_compras)
+
+
+for (let q = 0; q < lista_compras.length; q++) {
+    alert(lista_compras[q])
+} 
+
+
--- Javascript/gerenciador_de_notas.js
+++ Javascript/gerenciador_de_notas.js
@@ -26,6 +26,13 @@ const gerenciadorDeNotas = {
         gerenciadorDeNotas.notas[posicao] = {id: id, titulo: tituloEditado, conteudo: conteudoEditado };
 
         return gerenciadorDeNotas.notas;
+    },
+    excluirNota: function(id) {
+        const posicao = gerenciadorDeNotas.notas.findIndex((nota) => nota.id === id);
+        if(posicao < 0) {
+            return 'Nota não encontrada!';
+        }
+        gerenciadorDeNotas.notas.splice(posicao, 1)
     }
 }
 
@@ -36,9 +43,7 @@ gerenciadorDeNotas.adicionarNota(2, 'Estudar 4 aulas por dia!', 'Módulo 2 de JS
 gerenciadorDeNotas.adicionarNota(3, 'Lembrar de tomar café antes das aulas!', 'Você sempre deixa esfriar!');
 gerenciadorDeNotas.adicionarNota(4, 'Comprar novo teclado para estudar as aulas', 'Este está horrível');
 
-gerenciadorDeNotas.atualizarNota(2, 'nota editada', 'contedo editado');
-
-console.log(gerenciadorDeNotas.atualizarNota(4, 'nota editada', 'contedo editado'));
+console.log(gerenciadorDeNotas.excluirNota(1));
 
 //console.log(gerenciadorDeNotas.notas)
 
