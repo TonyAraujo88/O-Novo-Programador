40f8928ea244396818efe659701e1436df9efd65
TonyAraujo88 <tonybugattieb@gmail.com>
2025-03-02 11:44:35 -0300

    for_of_1

--- Javascript/for.js
+++ Javascript/for.js
@@ -5,40 +5,16 @@ const lista_compras = [
         "Farinha"
 ];
 
-lista_compras[2] = "Batata";
-console.log(lista_compras)
 
-const estudante = {
-    nome: 'Tony',
-    curso: 'HTML'
-};
-
-    let encontrouCurso = false;
-    
-    for(let chave in estudante) {
-        if(estudante[chave] === 'HTML') {
-            encontrouCurso = true;
-            break;
-        }
-    }
-
-    console.log(encontrouCurso);
-
-for ( let chave in lista_compras) {
-    if(lista_compras[chave] === 'Feijão') {
-        console.log('Encontrei');
-        break;        
-    } else {
-        console.log('Não encontrei')
-    }
+for(let chave in lista_compras) {
+    console.log(chave)
 }
 
+for(let item of lista_compras) {
+    console.log(item)
+}
 lista_compras.map((item) => {
-
-})
-
-for (let q = 0; q < lista_compras.length; q++) {
     
-} 
+}) 
 
 
