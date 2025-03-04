0271ca08d3ca7d85bc813c2e80fa8592cdb9fc2c
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-03-04 20:05:00 +0000

    desestructuring_obj

--- Javascript/desestruturing.js
+++ Javascript/desestruturing.js
@@ -1,6 +1,6 @@
 const usuario = {
     nome: 'Maria',
-    idade: 26,
+    idade: 25,
     cidade: 'São Paulo',
     email: 'maria@gmail.com',
     redes: {
@@ -10,10 +10,26 @@ const usuario = {
 };
 
 // Forma antiga
+
 const nomeUsuario = usuario.nome;
 const idadeUsuario = usuario.idade;
 
 // Com desesttruturing
 
-const { nome, idade} = usuario;
+const { nome, idade } = usuario;
 
+//console.log(nome)
+
+// proximo exemplo
+
+const pedido = {
+    numero: 12345,
+    cliente: 'Maria',
+    prato: 'Bolo',
+    endereco: 'Rua das flores',
+    valor: 50.00
+}
+
+const { numero, prato } = pedido;
+
+console.log(numero)
