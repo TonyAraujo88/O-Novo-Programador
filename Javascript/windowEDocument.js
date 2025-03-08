c2a883caa92b032a558aa223140b509b7a960ca0
TonyAraujo88 <tonybugattieb@gmail.com>
2025-03-08 01:36:25 -0300

    estrtra_dom

---
+++ Javascript/windowEDocument.js
@@ -0,0 +1,26 @@
+const dom = {
+    window: {
+        innerHeight: 400,
+        document: {
+            body: {
+                div: {
+                    div: {
+                        p: {
+                            classNome: [
+                                'pessoa',
+                                'desativada'
+                            ]
+                        }
+                    }
+                },
+                input: {
+                    type: 'text',
+                    name: 'nome',
+                    calssName: [
+                        'campo'
+                    ]
+                }
+            }
+        }
+    }
+}
