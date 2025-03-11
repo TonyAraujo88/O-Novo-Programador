c7d3d91d6b1147c886243489e7a02142b6c3ef0d
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-03-11 20:04:21 +0000

    manipulacao-dom2

--- Javascript/dom1.js
+++ Javascript/dom1.js
@@ -4,8 +4,12 @@ titulo.textContent = 'O Vitor é muito gente boa!';
 
 
 const elementosVerdes = document.getElementsByClassName('texto--verde')
-console.log(elementosVerdes)
+//console.log(elementosVerdes)
 
 for(let i = 0; i < elementosVerdes.length; i++) {
     elementosVerdes[i].style.color = 'blue';
+    elementosVerdes[i].style.fontSize = '50';
 }
+
+const titulos = document.getElementsByTagName('h1');
+console.log(titulos)
