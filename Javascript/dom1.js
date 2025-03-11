48ec547dd499e40a17534434be082c68db34d759
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-03-11 20:15:40 +0000

    dom2

--- Javascript/dom1.js
+++ Javascript/dom1.js
@@ -3,8 +3,14 @@ titulo.textContent = 'O Vitor é muito gente boa!';
 
 
 
-const elementosVerdes = document.getElementsByClassName('texto--verde')
+const elementosVerdes = document.getElementsByClassName('texto--verde');
 //console.log(elementosVerdes)
+
+const div = document.getElementById('espaco');
+
+div.style.height  = '100px';
+div.style.width = '100px';
+div.style.background = 'red';
 
 for(let i = 0; i < elementosVerdes.length; i++) {
     elementosVerdes[i].style.color = 'blue';
