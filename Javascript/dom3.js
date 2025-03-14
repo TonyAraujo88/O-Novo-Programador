d0221d6c3a8d420cce820a65e9128bb59b52aa88
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-03-14 17:02:15 +0000

    innerHTML

--- Javascript/dom3.js
+++ Javascript/dom3.js
@@ -1,12 +1,5 @@
 const titulo = document.querySelector('.titulo');
 
-titulo.setAttribute('class', 'titulo tema--amarelo')
+//titulo.setAttribute('class', 'titulo tema--amarelo')
 
-const logo = document.querySelector('img');
-
-logo.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS_E9-Q2C1lbhG6TAssR8QoTKGQaP6ONacLg&s')
-
-
-
-
-console.log(titulo)
+titulo.innerHTML = '<div style="font-size: 50">Vitor legal</div>'
