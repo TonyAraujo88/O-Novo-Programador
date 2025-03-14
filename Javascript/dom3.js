b6cbb050c1ccefa9849df952a9dc7b5e4ba797c0
Antonio Araújo <134899443+TonyAraujo88@users.noreply.github.com>
2025-03-14 16:52:21 +0000

    setAttribute_src

--- Javascript/dom3.js
+++ Javascript/dom3.js
@@ -2,4 +2,11 @@ const titulo = document.querySelector('.titulo');
 
 titulo.setAttribute('class', 'titulo tema--amarelo')
 
+const logo = document.querySelector('img');
+
+logo.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS_E9-Q2C1lbhG6TAssR8QoTKGQaP6ONacLg&s')
+
+
+
+
 console.log(titulo)
